f761fbc8143cb456ef2708f21a321ddd9c7700ab
michaldra <148788660+michaldra@users.noreply.github.com>
2024-05-14 19:32:54 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            "ROFL": "odpowiedź na żart",
+            "SHEESH": "lekka dezaprobata",
+            "CREEPY": "straszny, złowieszczy",
+            "AGGRO": "stać się agresywnym/zły",
+            }
+while True:
+    word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("Nie znaleziono słowa w naszym słowniku!")
